7137fd4201125e42c700dde64ababae3567ad06d
Gautam Kumar <gautamk8749@gmail.com>
2026-09-06 05:59:27 +0530

    Add files via upload

---
+++ firebase/firebase-config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoxZ4E_9cPSGQAJ9LoWWfpNMMwz3jI12g",
+  authDomain: "kirana-store-7266a.firebaseapp.com",
+  projectId: "kirana-store-7266a",
+  storageBucket: "kirana-store-7266a.firebasestorage.app",
+  messagingSenderId: "884573456752",
+  appId: "1:884573456752:web:ca15e3ce1d3f30c255a7b5",
+  measurementId: "G-1LF9BCQT7R"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
